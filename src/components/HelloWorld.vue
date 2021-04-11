08e88c5b86a89ebee732724fb3616097a6fbdf06
Masa <katsukatsu.masa.0522@gmail.com>
2021-04-11 10:46:15 +0900

    side navigationbar edit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,8 +1,20 @@
 <template>
   <v-app>
-    <v-app-bar color="#78909C" dark app>
+    <v-navigation-drawer class="font-weight-black" v-model="drawer" clipped app>Lists</v-navigation-drawer>
+    <v-app-bar color="#78909C" dark clipped-left app>
+      <v-app-bar-nav-icon @click="drawer=!drawer"></v-app-bar-nav-icon>
       <v-toolbar-title class="font-italic">Vuetify_share_house</v-toolbar-title>
     </v-app-bar>
     <v-footer color="#78909C" class="font-italic" dark app>Vuetify_share_house</v-footer>
   </v-app>
 </template>
+
+<script>
+  export default {
+    data() {
+      return{
+        drawer: null
+      }
+   }
+  } 
+</script>
