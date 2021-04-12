78c134e8f6e2b112f467f5b9e938cb450a2e563a
Masa <katsukatsu.masa.0522@gmail.com>
2021-04-12 19:54:57 +0900

    フォルダ作成

---
+++ src/views/Test2.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-container>
+    <h1>Test2</h1>
+    <router-link to="/Test2">Test2</router-link>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Test2"
+};
+</script>
+<style scoped>
+</style>
