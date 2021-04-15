ba7d962e37cf5e6e19cc24a3c66edfc16d3078d7
Masa <katsukatsu.masa.0522@gmail.com>
2021-04-15 16:33:13 +0900

    add

--- src/router.js
+++ src/router.js
@@ -4,11 +4,13 @@ import Home from './views/Home'
 import Test1 from './views/Test1'
 import Test2 from './views/Test2'
 import Counter from './components/Counter'
+import store from "@/store.js"
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
     mode: 'history',
+    base: process.env.BASE_URL,
     routes: [
       {
         path: '/',
@@ -29,7 +31,32 @@ export default new Router({
         path: '/Counter',
         name: 'Counter',
         component: Counter
+      },
+      {
+        path: "/sign-in",
+        name: "signin",
+        component: () => import("./views/Signin.vue")
+      },
+      {
+        path: "/join",
+        name: "join",
+        component: () => import("./views/Join.vue")
       }
     ]
-  })
-  
+  });
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.authRequired)) {
+    if (!store.state.isAuthenticated) {
+      next({
+        path: "/sign-in"
+      });
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
+});
+
+export default router;
