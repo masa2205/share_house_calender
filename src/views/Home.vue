fddd37a2f348ae9c13311b0a1c3b14f2b7678cd7
Masa <katsukatsu.masa.0522@gmail.com>
2021-04-15 10:32:35 +0900

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <span>
+    <hello-world/>
+  </span>
+</template>
+
+<script>
+import HelloWorld from "@/components/HelloWorld";
+
+export default {
+  name: "home",
+  components: {
+    HelloWorld,
+  }
+};
+</script>
