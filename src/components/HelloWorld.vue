b600f87560037246f2d38014d4a0917080a6d6bb
Masa <katsukatsu.masa.0522@gmail.com>
2021-04-15 10:32:51 +0900

    style change

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,15 +1,24 @@
 <template>
-  <v-app>
-    <v-img
-     lazy-src="https://picsum.photos/id/11/10/6"
-     max-height="auto"
-     max-width="auto"
-     src="https://picsum.photos/id/11/500/300">
-    </v-img>
-    <v-date-picker :mode="mode" :formats="formats" v-model="selectedDate"></v-date-picker>
-  </v-app>
+  <v-container fluid fill-height class="home-hero" style="max-height: 100vh;">
+    <v-layout justify-center align-center column pa-5>
+      <div class="display-4 font-weight-black white--text text-xs-center">Whelcome To</div>
+      <div class="display-4 font-weight-black white--text text-xs-center mb-3">Masa's House</div>
+      <div class="display-1 font-weight-bold white--text text-xs-center">good job!</div>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
-  
+export default {
+  name: "HelloWorlld"
+};
 </script>
+
+<style scoped>
+.home-hero {
+  background: url("https://picsum.photos/id/11/10/6");
+  background-size: cover;
+  width: 100%;
+  height: 250%;
+}
+</style>
