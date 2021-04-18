1f70f7a2669106e9a4bfee0e4dc528aea729451e
Masa <katsukatsu.masa.0522@gmail.com>
2021-04-18 13:36:06 +0900

    v-contet add

--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -1,27 +1,31 @@
 <template>
-    <v-list three-line>
-      <template v-for="(comment, index) in comments">
-        <v-list-item
-            :key="index"
-            avatar
-        >
-          <v-list-item-avatar>
-            <img :src="comment.avatar">
-          </v-list-item-avatar>
-  
-          <v-list-item-content>
-            <v-list-item-subtitle class="text--primary subheading">{{comment.content}}</v-list-item-subtitle>
-            <v-list-item-subtitle>
-              {{comment.createdAt.toDate().toLocaleString()}}
-            </v-list-item-subtitle>
-          </v-list-item-content>
-  
-          <v-list-item-action>
-          </v-list-item-action>
-        </v-list-item>
-        <v-divider :key="comment.id"></v-divider>
-      </template>
-    </v-list>
+  <v-content>
+    <v-container fluid fill-height>
+      <v-list three-line>
+        <template v-for="(comment, index) in comments">
+          <v-list-item
+              :key="index"
+              avatar
+          >
+            <v-list-item-avatar>
+              <img :src="comment.avatar">
+            </v-list-item-avatar>
+    
+            <v-list-item-content>
+              <v-list-item-subtitle class="text--primary subheading">{{comment.content}}</v-list-item-subtitle>
+              <v-list-item-subtitle>
+                {{comment.createdAt.toDate().toLocaleString()}}
+              </v-list-item-subtitle>
+            </v-list-item-content>
+    
+            <v-list-item-action>
+            </v-list-item-action>
+          </v-list-item>
+          <v-divider :key="comment.id"></v-divider>
+        </template>
+      </v-list>
+    </v-container>  
+  </v-content>    
 </template>
 
 <script>
