257c135f7f6564c8725500e90b588e4bbff397fe
Masa <katsukatsu.masa.0522@gmail.com>
2021-04-22 20:39:54 +0900

    img edit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container fluid fill-height class="home-hero" style="max-height: 100vh;">
-    <v-layout justify-center align-center column pa-5>
+    <v-layout justify-center align-left column pa-5>
       <div class="display-4 font-weight-black white--text text-xs-center">Whelcome To</div>
       <div class="display-4 font-weight-black white--text text-xs-center mb-3">Masa's House</div>
       <div class="display-1 font-weight-bold white--text text-xs-center">good job!</div>
@@ -16,7 +16,7 @@ export default {
 
 <style scoped>
 .home-hero {
-  background: url("https://picsum.photos/id/11/10/6");
+  background: url("https://user-images.githubusercontent.com/65245621/115658257-6a9a5d80-a373-11eb-9a14-74a8c10e435d.jpg");
   background-size: cover;
   width: 100%;
   height: 250%;
